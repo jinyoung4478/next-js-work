fbed5c71383e42f0c6ba176c7ef4b6942b325298
김진영 <skswls0@daum.net>
2023-01-17 21:40:54 +0900

    feat: Movie API 적용 및 rewrites 실습

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+const nextConfig = {
+   reactStrictMode: true,
+   async redirects() {
+      return [
+         {
+            source: '/test/:test',
+            destination: '/test-result/:test',
+            permanent: false,
+         },
+      ];
+   },
+   async rewrites() {
+      return [
+         {
+            source: '/api/movies',
+            destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`,
+         },
+      ];
+   },
+};
+
+module.exports = nextConfig;
