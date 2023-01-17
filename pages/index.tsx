59ec50c03b3aebc51f87ea463dd8e802731448a4
김진영 <skswls0@daum.net>
2023-01-18 01:14:38 +0900

    fix: Image path 수정 및 pointer 효과 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({ movies }: InferGetServerSidePropsType<GetServerSi
 			{!movies && <h4>Loading</h4>}
 			{movies?.map((movie: IMovieProps) => (
 				<div className="movie" key={movie.id}>
-					<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+					<img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
 					<h4>{movie.original_title}</h4>
 				</div>
 			))}
@@ -36,6 +36,9 @@ export default function Home({ movies }: InferGetServerSidePropsType<GetServerSi
 					grid-template-columns: 1fr 1fr;
 					padding: 20px;
 					gap: 20px;
+				}
+				.movie {
+					cursor: pointer;
 				}
 				.movie img {
 					max-width: 100%;
