2b7c790e722b857752ed8ac8b8d5c49937198bf5
김진영 <skswls0@daum.net>
2023-01-17 20:34:44 +0900

    feat: Initialize project

---
+++ components/NavBar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <Link href="/">
+        <span>Home</span>
+      </Link>
+      <Link href="/about">
+        <span>About Us</span>
+      </Link>
+      <style jsx global>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+      `}</style>
+    </nav>
+  );
+}
